5374a4752b8cb4799eedb16df594e6ddbdfda51d
CarolGitonga <carolgitongaofficial@gmail.com>
2024-05-06 21:43:32 +0300

    create housing location interface

---
+++ src/app/housinglocation/housinglocation.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-housinglocation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './housinglocation.component.html',
+  styleUrl: './housinglocation.component.css'
+})
+export class HousinglocationComponent {
+  @Input() housingLocation!: Housinglocation;
+
+}
