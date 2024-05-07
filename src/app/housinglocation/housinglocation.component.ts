8fa361a961bc6862a9fd2a5bb67afa76cc95a2e1
CarolGitonga <carolgitongaofficial@gmail.com>
2024-05-07 13:13:35 +0300

    create housing sevices

--- src/app/housinglocation/housinglocation.component.ts
+++ src/app/housinglocation/housinglocation.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Housinglocation } from '../housinglocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housinglocation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+
+  ],
   templateUrl: './housinglocation.component.html',
   styleUrl: './housinglocation.component.css'
 })
