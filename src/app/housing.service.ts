61a674e22b707bf9762b0fabbafd72294bb42b88
CarolGitonga <carolgitongaofficial@gmail.com>
2024-05-07 14:16:08 +0300

    add a form and its functionalities

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Housinglocation } from './housinglocation';
+import e from 'express';
 
 @Injectable({
   providedIn: 'root'
@@ -118,5 +119,9 @@ export class HousingService {
   getHousingLocationById(id: number): Housinglocation | undefined {
     return this.housingLocationList.find(housingLocation => 
       housingLocation.id ===id);
+  }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName:
+    ${firstName}, lastName: ${lastName}, email: ${email}.`);
   }
 }
